24794465e340ccd807d78aa9ffeb0d827785f66a
rohitanand105 <rohitanand105@gmail.com>
2025-04-30 15:03:04 +0530

    new

---
+++ src/middlewares/validationMiddleware.js
@@ -0,0 +1,27 @@
+const { body, validationResult } = require('express-validator');
+
+// Validation rules
+exports.validateRegister = [
+  body('username').notEmpty().withMessage('Username is required'),
+  body('email').isEmail().withMessage('Invalid email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success: false, errors: errors.array() });
+    }
+    next();
+  },
+];
+
+exports.validateLogin = [
+  body('email').isEmail().withMessage('Invalid email'),
+  body('password').notEmpty().withMessage('Password is required'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success: false, errors: errors.array() });
+    }
+    next();
+  },
+];
